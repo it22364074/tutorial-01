f29f8487a5c8d1d9724f2aa7ca0bd2f0f0e1d27b
it22364074 <127375108+it22364074@users.noreply.github.com>
2023-03-09 08:17:27 +0000

    commit

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,20 @@
 #include <stdio.h>
 
 int main() {
+      int mark1,mark2;
+      float sum,average;
+
+
+  printf("Input Mark 01:");
+  scanf("%d",&mark1);
+      
+   printf("Input Mark 02:");
+   scanf("%d",&mark2);
+
+   sum=mark1+mark2;;
   
+   average=(sum/2);
+   printf("Average:%2f",average);
   return 0;
 }
 
--- Tute03.c
+++ Tute03.c
@@ -13,7 +13,13 @@ sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
 int main() {
-  
+    int n,i;
+    int count;
+   printf("Input n:");
+   scanf("%d",&n);
+
+   for(i=1)
+    
   return 0;
 }
 
